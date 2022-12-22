7e1550d1adf6a057e7b16f2ef61449fa477bc3d4
Narasimman2067 <narasimmamech2067@gmail.com>
2022-12-22 08:11:53 +0530

    update_notes

--- DOM-Events/script.js
+++ DOM-Events/script.js
@@ -1,46 +1,177 @@
-// document.body.textContent="im a new document";
+//  document.body.textContent="im a new document";
 // console.log(document)
 
 
-// // bom BOM
-// console.log(this);
-// // empty javascript file
-// // is a minimum function  of a javascript
+// // // bom BOM
+//  console.log(this);
+// // // empty javascript file
+// // // is a minimum function  of a javascript
 
 // console.log(window);
-// // window is a simple object that shows the whole window
-// window.alert("hi");
+// // // window is a simple object that shows the whole window
+//  window.alert("hi");
 
-// const log =() =>
-// {
-//     console.log("hi there");
+//  const log =() =>
+//  {     console.log("hi there");
 
 // }
 // const windowObject={
 //     logger : log(),
 // }
 // windowObject.logger;
-for (let i =0;i<6;i++)
-{
-    // if(i==3)
-    // {
-    //     // confirm(" hey the loop has runed three times");
-    // }
-    console.log(i);
-}
-console.log(window);
+// for (let i=0;i<6;i++)
+// {
+//     if(i==3)
+//     {
+//          confirm(" hey the loop has runed three times");
+//     }
+//     console.log(i);
+// }
+// console.log(window);
 
-    function newFunction(){
-        console.log("hey i'm the function")
-        alert('hi there');
-    }
+//     function newFunction(){
+//         console.log("hey i'm the function")
+//         alert('hi there');
+//     }
 
-    function newSecons(){
-        console.log("hey i'm the function");
-        // alert(" hey i'm the function")
+//     function newSecons(){
+//         console.log("hey i'm the function");
+//         // alert(" hey i'm the function")
        
+//     }
+//     setTimeout(newSecons,1000);
+
+    //alert
+    //prompt
+    //confirm
+
+
+// alert // prompt //confirm
+
+//////////alert ////////////////
+
+function alertFunction(message) {
+    alert(message); // nothing
+  }
+  
+  /////////promt////////////////
+  let promptElement = document.createElement("div")
+  document.body.append(promptElement); 
+  
+  function promptFunction(){
+    let answer = prompt("Hey there have you attended the class?", "") // returns a null or value
+   
+      promptElement.textContent = `${answer} is your answer`
+    
+  }
+  
+  //////////confrim////////////////////
+  
+  function confirmFunction(){
+    let response = confirm("Are you confirming?"); // true or false
+  
+    if (response == true){
+      promptElement.textContent = `You have approved the request`
+    } else {
+      promptElement.textContent = `You have rejected the request`
     }
-    setTimeout(newSecons,5000);
+  }
+  
+  /////////set time out////////////////
+  
+  let count = 10; 
+  
+  function timeOutExample(){
+     // count++ or count = count +1 
+    promptElement.textContent = count; 
+    count = count-1;
+    const id = setTimeout(timeOutExample, 1000); 
+    if (count == 0){
+      clearTimeout(id); 
+      promptElement.textContent = "Your time is up..."
+    }
+  }
+  
+  
+  ///////////set interval//////////////
+  // repeat after certain time 
+  function intervalFunction(){
+  let ourInterval = setInterval(() => {
+     promptElement.textContent = count
+     count = count - 1
+     if (count === 0 ){
+      clearInterval(ourInterval)
+      promptElement.textContent = "your time has stopped..."
+     }
+  }, 1000);
+  }
+  
+  /////////Events////////
+  let clickCounter = 0
+  
+  function clickEvent(){
+    clickCounter++;
+    console.log("You clicked this", clickCounter)
+   const inputVarible = document.getElementById("input-id"); 
+   console.log(inputVarible.value);
+   inputVarible.value = "";
+   
+  }
+  let changeCounter = 0
+  function changeEvent(){
+    changeCounter++;
+    console.log("You changed this", changeCounter)
+  }
+  
+  
+  let addBtn = document.getElementById("btn-id")
+  /////
+  addBtn.addEventListener("click", ()=>{
+    console.log("you have created a function")
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
         
--- script.js
+++ script.js
@@ -1,84 +1,155 @@
 
-// console.log(document);
-// let elementName= prompt("hai tell me what you feel", "")
-// if(elementName != null){
-// newElement.innerText=elementName;
+// // console.log(document);
+// // let elementName= prompt("hai tell me what you feel", "")
+// // if(elementName != null){
+// // newElement.innerText=elementName;
+// // }
+// let newElement = document.createElement("div")
+
+// // newElement.innerText="i am from JS"
+// newElement.innerText="i'm a inner text"
+// newElement.style.backgroundColor="yellow";
+// newElement.style.color="red";
+// newElement.innerText="i'm a inner text"
+
+
+// // inner text
+// newElement.innerText
+// newElement.innerHTML =
+// ` <h1>hi there</h1>
+// <div>
+// <h1 style="background-color:red;color:yellow;font-family:algerian">I'm a inner html</h1>
+// </div>`
+
+// // set attribute
+// newElement.setAttribute("class","create-class")
+
+// //  get element by id
+// let inbuildElement=document.getElementById("sample-id");
+// inbuildElement.innerText="JS intro over Id";
+// inbuildElement.style.backgroundColor="red";
+// var queryElement=document.querySelector('#sample-id')
+
+
+
+
+// // sample  element by id
+
+// let demoPara=document.getElementById("para");
+
+// //here i changed the text content by using the text content element
+// // see in the front html 26 line i had changed that line by giving new text content 
+// demoPara.textContent="I'm changed to         new text content";
+// // inner text
+// console.log("inner text:",demoPara.innerText);
+// //text content
+// console.log("text content: ",demoPara.textContent);
+
+// // query selector and query selector all
+// var queryElement=document.querySelector('.sample-class')
+// queryElement.style.color="blue"
+// console.log(queryElement);
+
+// // query selector all
+// var queryAllElement=document.querySelectorAll(".sample-class")
+// console.log(queryAllElement);
+// for(let i=0;i<queryAllElement.length;i++)
+// {
+    
+//     queryAllElement[i].style.color="green";
+//     queryAllElement[i].innerText="js class over";
+//     console.log(queryAllElement[i]);
 // }
-let newElement = document.createElement("div")
-
-// newElement.innerText="i am from JS"
-newElement.innerText="i'm a inner text"
-newElement.style.backgroundColor="yellow";
-newElement.style.color="red";
-newElement.innerText="i'm a inner text"
 
 
-// inner text
-newElement.innerText
-newElement.innerHTML =
-` <h1>hi there</h1>
+// document.body.append(newElement);
+
+// console.log( "initial :",newElement);
+
+// const newDivision=document.createElement("div");
+
+// newDivision.setAttribute("id","append-sample");
+
+// newDivision.textContent="i'm from new append";
+// //  we can use append and create and use multiple elements like an array
+
+// newElement.append(newDivision,"hey there i'm a new string",demoPara);
+
+// // in append child we can use only the element not like strings,values had not been included
+
+// newElement.appendChild(newDivision);
+
+
+// console.log("final :",newElement);
+
+// creating a element 
+// div span p h1, h6, mark0 --- elements;
+
+
+let newElement = document.createElement("div"); 
+newElement.style.backgroundColor = "yellow"
+newElement.style.color = "red"
+//inner text
+newElement.innerText = "I won't work when there is a inner html"
+
+
+ //inner html
+newElement.innerHTML = 
+`
+HI there
 <div>
-<h1 style="background-color:red;color:yellow;font-family:algerian">I'm a inner html</h1>
-</div>`
-
+<h1 style="background-color: red; color:yellow">I'm inner html</h1>
+</div>
+`
 // set attribute
-newElement.setAttribute("class","create-class")
+newElement.setAttribute("class", "create-class")
 
-//  get element by id
-let inbuildElement=document.getElementById("sample-id");
-inbuildElement.innerText="JS intro over Id";
-inbuildElement.style.backgroundColor="red";
-var queryElement=document.querySelector('#sample-id')
-
-
-
+// get elementby id
+let inbuildElement = document.getElementById("sample-id"); 
+inbuildElement.innerText= "Js intro over Id"
+inbuildElement.style.backgroundColor = "red";
 
 // sample  element by id
 
-let demoPara=document.getElementById("para");
+let demoPara = document.getElementById("para")
 
-//here i changed the text content by using the text content element
-// see in the front html 26 line i had changed that line by giving new text content 
-demoPara.textContent="I'm changed to         new text content";
-// inner text
-console.log("inner text:",demoPara.innerText);
+// inner text 
+console.log("inner text: ", demoPara.innerText);
 //text content
-console.log("text content: ",demoPara.textContent);
+console.log("text content: ", demoPara.textContent)
 
-// query selector and query selector all
-var queryElement=document.querySelector('.sample-class')
-queryElement.style.color="blue"
+
+
+
+// query selector and query selector all..
+var queryElement = document.querySelector('.sample-class')
+queryElement.style.color ="blue"
 console.log(queryElement);
 
-// query selector all
-var queryAllElement=document.querySelectorAll(".sample-class")
+// query selector all 
+var queryAllElement = document.querySelectorAll(".sample-class");
+
 console.log(queryAllElement);
-for(let i=0;i<queryAllElement.length;i++)
-{
-    
-    queryAllElement[i].style.color="green";
-    queryAllElement[i].innerText="js class over";
+
+for (let i = 0; i<queryAllElement.length; i++){
     console.log(queryAllElement[i]);
+    queryAllElement[i].style.color = "green"
+    queryAllElement[i].innerText = "js class over"
 }
+
 
 
 document.body.append(newElement);
 
-console.log( "initial :",newElement);
 
-const newDivision=document.createElement("div");
+const newDivision = document.createElement("div")
 
-newDivision.setAttribute("id","append-sample");
-
-newDivision.textContent="i'm from new append";
-//  we can use append and create and use multiple elements like an array
-
-newElement.append(newDivision,"hey there i'm a new string",demoPara);
-
-// in append child we can use only the element not like strings,values had not been included
-
-newElement.appendChild(newDivision);
+newDivision.setAttribute("id", "apped-sample");
+newDivision.textContent = "i'm from new append"
 
 
-console.log("final :",newElement);
+newElement.append(newDivision, "hey there i'm  a new string" )
+//newElement.appendChild(newDivision, "hey there i'm  a new string");
+console.log("final :", newElement)
 
+[1, 2, 3, 4, 5]
